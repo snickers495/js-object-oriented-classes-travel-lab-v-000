51339ebfa83155a93e2966d92dd0b811947f2523
Lisa Lee <snickers495@gmail.com>
2018-01-14 18:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  class Driver {
    constructor(name, startDate){
      this.name = name;
-     this.startDate = startDate;
+     this.startDate = Date.parse(startDate);
    }
    yearsExperienceFromBeginningOf(year) {
      return year - this.startDate;
