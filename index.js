ab6ee102c4326ba06c5f96213b451952292b5a0a
Lisa Lee <snickers495@gmail.com>
2018-01-14 18:25:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@
  class Route {
    constructor(beginningLocation, endingLocation){
      this.beginningLocation_horizontal = beginningLocation.horizontal;
-     this.beginningLocation_horizontal = beginningLocation.horizontal;
+     this.beginningLocation_verticla = beginningLocation.vertical;
      this.endingLocation_horizontal = endingLocation.horizontal;
-     this.beginningLocation_horizontal = beginningLocation.horizontal;
+     this.endingLocation_vertical= endingLocation.vertical;
    }
    blocksTravelled(){
 
